bb07b4b56e51797193765cb505ab9ffdf998a7b9
SoumyadeepGhosh16 <soumyadeepghosh.s123@gmail.com>
2024-07-21 15:35:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+
+
+
+const App = () => {
+
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
